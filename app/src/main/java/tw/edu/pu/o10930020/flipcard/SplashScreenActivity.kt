2cdf138575cb776543faaa5a20504b2861e35afa
Hanson Angelino <www.hanstopz@gmail.com>
2022-06-23 09:27:29 +0800

    SplashScreen-Done

--- app/src/main/java/tw/edu/pu/o10930020/flipcard/SplashScreenActivity.kt
+++ app/src/main/java/tw/edu/pu/o10930020/flipcard/SplashScreenActivity.kt
@@ -1,11 +1,21 @@
 package tw.edu.pu.o10930020.flipcard
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashScreenActivity : AppCompatActivity() {
+
+    private val splashScreentimeout: Long = 2500
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, splashScreentimeout)
     }
 }
