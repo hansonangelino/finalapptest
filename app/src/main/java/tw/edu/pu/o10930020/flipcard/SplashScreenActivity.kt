10406fb39af1dc0896e5fa467bde20924ae15a2a
Hanson Angelino <www.hanstopz@gmail.com>
2022-06-23 09:05:54 +0800

    SplashScreen

---
+++ app/src/main/java/tw/edu/pu/o10930020/flipcard/SplashScreenActivity.kt
@@ -0,0 +1,11 @@
+package tw.edu.pu.o10930020.flipcard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
